0b1f7d83fd691bbe5b4607f6eecbb1fb9a749533
Foxicution <foxicution@gmail.com>
2024-02-18 19:04:30 +0000

    simple moving square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const GRID_SIZE: f32 = 20.0;
+
+#[macroquad::main("MacroSnake")]
+async fn main() {
+    let (mut pos_x, mut pos_y) = (0.0, 0.0);
+
+    loop {
+        // match get_last_key_pressed() {
+        //     Some(KeyCode::H) => pos_x -= GRID_SIZE,
+        //     Some(KeyCode::L) => pos_x += GRID_SIZE,
+        //     Some(KeyCode::J) => pos_y -= GRID_SIZE,
+        //     Some(KeyCode::K) => pos_y += GRID_SIZE,
+        //     _ => {}
+        // }
+        if is_key_down(KeyCode::H) {
+            pos_x -= GRID_SIZE;
+        } else if is_key_down(KeyCode::L) {
+            pos_x += GRID_SIZE;
+        } else if is_key_down(KeyCode::J) {
+            pos_y -= GRID_SIZE;
+        } else if is_key_down(KeyCode::K) {
+            pos_y += GRID_SIZE;
+        }
+        clear_background(BLACK);
+        draw_rectangle(pos_x, pos_y, GRID_SIZE, GRID_SIZE, GREEN);
+        next_frame().await
+    }
 }
